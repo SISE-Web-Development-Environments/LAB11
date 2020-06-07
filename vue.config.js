791dabb0bb85e0bde2e6103ef1eceb180b252461
NAre5 <sherlord5@gmail.com>
2020-06-08 00:18:08 +0300

    end of 7/6/20

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   configureWebpack: {
     devtool: "source-map"
+  },
+  devServer: {
+    host: "localhost"
   }
-  // publicPath: process.env.NODE_ENV === "production" ? "/dist/" : "/"
 };
